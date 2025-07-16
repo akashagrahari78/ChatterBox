f55566120810b211f93b6a5395838af5cc73b98f
Akash Agrahari <akashagrahari746@gmail.com>
2025-07-16 17:54:46 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.send("you are in home page")
+
+})
+
+app.listen(3000, ()=>{
+    console.log("server running in port 3000")
+    
+})
